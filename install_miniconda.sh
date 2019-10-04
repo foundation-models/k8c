b9d071fbdddaf889edf925b8368fe34c829a1271
hossein20s <hossein.objectj@gmail.com>
2019-10-04 23:53:39 +0000

    adding shell scripts

---
+++ install_miniconda.sh
@@ -0,0 +1,3 @@
+apt-get update; apt-get install -y wget bzip2
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
---
+++ install_mlpipeline.sh
@@ -0,0 +1,2 @@
+conda create --name mlpipeline python=3.7
+conda activate mlpipeline
